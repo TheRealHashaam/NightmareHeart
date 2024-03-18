06f135f9545157d43c0f677b4d7b00ffd77a1447
TheRealHashaam <hashaam@live.com>
2024-03-18 20:49:07 +0000

    health

---
+++ Assets/Scripts/HeartBeat/HealthBar.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+    public Transform target;
+    private TrailRenderer trailRenderer;
+    public float straightDuration = 2f;
+    public float pulseDuration = 1f;
+    public float normalspeed = 2f;
+    public float pulsespeed;
+    public float amplitude = 1f;
+    public float frequency = 5f;
+    private Vector3 startPosition;
+    public RectTransform endPos;
+    private float timer = 0f;
+    private bool isStraight = true;
+    public Color FullHealth;
+    public Color MidHealth;
+    public Color LowHealth;
+    void Start()
+    {
+        trailRenderer = target.GetComponent<TrailRenderer>();
+        startPosition = target.position;
+    }
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (isStraight)
+        {
+            float xOffset = normalspeed * Time.deltaTime;
+            Vector3 newPosition = target.position + new Vector3(xOffset, 0f, 0f);
+            target.position = newPosition;
+            if (timer >= straightDuration)
+            {
+                timer = 0f;
+                isStraight = false;
+            }
+        }
+        else
+        {
+            float xOffset = pulsespeed * Time.deltaTime;
+            float yOffset = Mathf.Sin(frequency * timer * Mathf.PI * 2f) * amplitude * Time.deltaTime;
+
+            Vector3 newPosition = target.position + new Vector3(xOffset, yOffset, 0f);
+
+            target.position = newPosition;
+
+            if (timer >= pulseDuration)
+            {
+                timer = 0f;
+                isStraight = true;
+            }
+        }
+        if(target.position.x>endPos.position.x)
+        {
+            target.position = startPosition;
+        }
+    }
+}
