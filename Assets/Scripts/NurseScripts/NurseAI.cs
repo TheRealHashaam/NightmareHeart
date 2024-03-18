dada5fdb34c6d364e31a91acad30c4b129ccd86e
TheRealHashaam <hashaam@live.com>
2024-03-18 13:16:00 +0000

    Ai

--- Assets/Scripts/NurseScripts/NurseAI.cs
+++ Assets/Scripts/NurseScripts/NurseAI.cs
@@ -3,18 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 using BehaviorTree;
 using UnityEngine.AI;
-public class NurseAI : BehaviorTree.Tree
+public class NurseAI : MonoBehaviour
 {
-    protected override Node SetupTree()
-    {
-        NavMeshAgent navMeshAgent = GetComponent<NavMeshAgent>();
-        if (navMeshAgent == null)
-        {
-            Debug.LogError("NavMeshAgent component is missing on NurseAI GameObject.");
-            return null;
-        }
-        Node root = new Patrol(navMeshAgent);
+    private NavMeshAgent agent;
+    private Vector3 patrolDestination;
+    private float patrolRange = 40f; // Adjust this value as needed
+    private float waitTime = 3f; // Adjust this value as needed
 
-        return root;
+    private bool waiting = false;
+    private Coroutine waitCoroutine;
+    Animator _animator;
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        _animator = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        if (!waiting)
+        {
+            if (!agent.pathPending && agent.remainingDistance < 0.5f)
+            {
+                Vector3 randomDirection = Random.insideUnitSphere * patrolRange;
+                randomDirection += agent.transform.position;
+                
+
+                waitCoroutine = StartCoroutine(WaitBeforeNextDestination(randomDirection));
+            }
+        }
+    }
+
+    private IEnumerator WaitBeforeNextDestination(Vector3 randomDirection)
+    {
+        waiting = true;
+        _animator.SetBool("Idle", true);
+        _animator.SetBool("Walk", false);
+        yield return new WaitForSeconds(waitTime);
+        NavMeshHit hit;
+        NavMesh.SamplePosition(randomDirection, out hit, patrolRange, NavMesh.AllAreas);
+        patrolDestination = hit.position;
+        agent.SetDestination(patrolDestination);
+        waiting = false;
+        _animator.SetBool("Idle", false);
+        _animator.SetBool("Walk", true);
     }
 }
